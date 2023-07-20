8df3141f54157e0b37217a18a8388fdfa4e35d84
IMxSCARED <brianmcnamee7503@gmail.com>
2023-07-20 16:49:24 +1000

    Added in sounds to the demo world scene game

---
+++ Raccoon Game/DarkSteamPunkgame/Assets/Scripts/FootStepScript.cs
@@ -0,0 +1,34 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootStepScript : MonoBehaviour
+{
+   public AudioSource walking, Running;
+
+    void Update()
+    {
+ 
+
+        if ( Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        {
+            walking.enabled = true;
+
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                walking.enabled = false;
+                Running.enabled = true;
+            }
+            else
+            {
+                Running.enabled = false;
+            }
+        }
+        else
+        {
+            walking.enabled = false;
+            Running.enabled = false;
+        }
+    }
+}
