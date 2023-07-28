99a2c5382dd6c3fc0dc2c11a07f52b173bf2d942
IMxSCARED <brianmcnamee7503@gmail.com>
2023-07-28 11:36:51 +1000

    added in UI for the bins collectd, added in scripts

---
+++ Raccoon Game/DarkSteamPunkgame/Assets/Scripts/UIInventory/PlayerInventory.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerInventory : MonoBehaviour
+{
+    public int NumberOfDiamonds { get; private set; }
+
+    public UnityEvent<PlayerInventory> OnDiamondCollected;
+
+    public void DiamondCollected()
+    {
+        NumberOfDiamonds++;
+        OnDiamondCollected.Invoke(this);
+    }
+}
